12a53311647f0c07d9214f003179872e8a6051ea
serya012 <ayrescarneiro01@gmail.com>
2024-01-22 14:54:24 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,5 @@ export class AppComponent {
     { title: 'Trash', url: '/folder/trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/spam', icon: 'warning' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
