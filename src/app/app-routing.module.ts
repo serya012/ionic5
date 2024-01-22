3c378d3b25804bc7ef412c426dcb67bc9a1a94b3
serya012 <ayrescarneiro01@gmail.com>
2024-01-22 20:28:05 -0300

    mudei o sistema de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,39 +4,35 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: '',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
-  {
     path: 'evento',
-    loadChildren: () => import('./pagina/evento/evento.module').then( m => m.EventoPageModule)
+    loadChildren: () => import('./pagina/evento/evento.module').then(m => m.EventoPageModule)
   },
   {
     path: 'orcamento',
-    loadChildren: () => import('./pagina/orcamento/orcamento.module').then( m => m.OrcamentoPageModule)
+    loadChildren: () => import('./pagina/orcamento/orcamento.module').then(m => m.OrcamentoPageModule)
   },
   {
     path: 'feedback',
-    loadChildren: () => import('./pagina/feedback/feedback.module').then( m => m.FeedbackPageModule)
+    loadChildren: () => import('./pagina/feedback/feedback.module').then(m => m.FeedbackPageModule)
   },
   {
     path: 'convidado',
-    loadChildren: () => import('./pagina/convidado/convidado.module').then( m => m.ConvidadoPageModule)
+    loadChildren: () => import('./pagina/convidado/convidado.module').then(m => m.ConvidadoPageModule)
   },
   {
     path: 'perfil',
-    loadChildren: () => import('./pagina/perfil/perfil.module').then( m => m.PerfilPageModule)
+    loadChildren: () => import('./pagina/perfil/perfil.module').then(m => m.PerfilPageModule)
   },
   {
     path: 'configuracao',
-    loadChildren: () => import('./pagina/configuracao/configuracao.module').then( m => m.ConfiguracaoPageModule)
+    loadChildren: () => import('./pagina/configuracao/configuracao.module').then(m => m.ConfiguracaoPageModule)
   }
-
 ];
+
 
 @NgModule({
   imports: [
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
-import { home, person, settings } from 'ionicons/icons';
+import { calendar, wallet, clipboard, people, person, settings } from 'ionicons/icons';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  home = home;
+  calendar = calendar;
+  wallet = wallet;
+  clipboard = clipboard;
+  people = people;
   person = person;
   settings = settings;
+
   public appPages = [
-    { title: 'Eventos', url: '/folder/Eventos', icon: 'calendar' },
-    { title: 'Orçamento', url: '/folder/orcamento', icon: 'wallet' },
-    { title: 'Feedback', url: '/folder/feedback', icon: 'clipboard' },
-    { title: 'Convidados', url: '/folder/convidados', icon: 'people' },
-    { title: 'Perfil', url: '/folder/perfil', icon: 'person' },
-    { title: 'Configuração', url: '/folder/configuracao', icon: 'settings' },
+    { title: 'Eventos', url: '/evento', icon: 'calendar' },
+    { title: 'Orçamento', url: '/orcamento', icon: 'wallet' },
+    { title: 'Feedback', url: '/feedback', icon: 'clipboard' },
+    { title: 'Convidados', url: '/convidado', icon: 'people' },
+    { title: 'Perfil', url: '/perfil', icon: 'person' },
+    { title: 'Configuração', url: '/configuracao', icon: 'settings' },
   ];
+
   constructor() {}
 }
