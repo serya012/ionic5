6777f6f428b52c219f1da9837973fad6b377f13d
serya012 <ayrescarneiro01@gmail.com>
2024-01-24 19:49:08 -0300

    feedback feito
    precisa de algumas alterações

--- src/app/pagina/feedback/feedback.module.ts
+++ src/app/pagina/feedback/feedback.module.ts
@@ -1,19 +1,21 @@
+// Importe o módulo StarRatingModule no seu módulo Angular
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { FeedbackPageRoutingModule } from './feedback-routing.module';
-
 import { FeedbackPage } from './feedback.page';
+
+// Importe o módulo StarRatingModule do pacote angular-star-rating
+import { StarRatingModule } from 'angular-star-rating';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FeedbackPageRoutingModule
+    FeedbackPageRoutingModule,
+    StarRatingModule.forRoot(), // Adicione esta linha para importar o módulo de estrelas
   ],
   declarations: [FeedbackPage]
 })
--- src/app/pagina/feedback/feedback.page.ts
+++ src/app/pagina/feedback/feedback.page.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+// feedback.page.ts
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.page.html',
   styleUrls: ['./feedback.page.scss'],
 })
-export class FeedbackPage implements OnInit {
+export class FeedbackPage {
+  seuNome: string = '';
+  feedback: string = '';
+  avaliacao: number = 0;
+  stars: number[] = [1, 2, 3, 4, 5];
 
-  constructor() { }
-
-  ngOnInit() {
+  setRating(event: any): void {
+    const selectedValue = event.target.getAttribute('data-value');
+    this.avaliacao = selectedValue ? +selectedValue : 0;
   }
 
+  enviarFeedback(): void {
+    // Lógica para enviar o feedback e avaliação
+    console.log('Nome:', this.seuNome);
+    console.log('Avaliação:', this.avaliacao);
+    console.log('Feedback:', this.feedback);
+  }
 }
