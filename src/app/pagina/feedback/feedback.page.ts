a2e35c6bb6ffff55d562677ebe7e6409689c3d10
serya012 <ayrescarneiro01@gmail.com>
2024-01-24 20:00:25 -0300

    lógica de avaliação feita

--- src/app/pagina/feedback/feedback.page.ts
+++ src/app/pagina/feedback/feedback.page.ts
@@ -1,5 +1,6 @@
 // feedback.page.ts
 import { Component } from '@angular/core';
+import { ToastController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-feedback',
@@ -11,16 +12,76 @@ export class FeedbackPage {
   feedback: string = '';
   avaliacao: number = 0;
   stars: number[] = [1, 2, 3, 4, 5];
+  formularioEnviado: boolean = false;
+
+  constructor(
+    private toastController: ToastController,
+    private alertController: AlertController
+  ) {}
 
   setRating(event: any): void {
     const selectedValue = event.target.getAttribute('data-value');
     this.avaliacao = selectedValue ? +selectedValue : 0;
   }
 
-  enviarFeedback(): void {
+  async confirmarEnvio(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Confirmação',
+      message: 'Tem certeza de que deseja enviar o formulário?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        },
+        {
+          text: 'Enviar',
+          handler: () => this.enviarFeedback(),
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async enviarFeedback(): Promise<void> {
+    // Verifica se a avaliação é maior que 0
+    if (this.avaliacao <= 0) {
+      const alert = await this.alertController.create({
+        header: 'Avaliação Inválida',
+        message: 'Por favor, escolha uma avaliação de 1 a 5 estrelas.',
+        buttons: ['OK'],
+      });
+
+      await alert.present();
+      return; // Não prossegue com o envio do formulário
+    }
+
     // Lógica para enviar o feedback e avaliação
     console.log('Nome:', this.seuNome);
     console.log('Avaliação:', this.avaliacao);
     console.log('Feedback:', this.feedback);
+
+    // Limpar o formulário após o envio
+    this.seuNome = '';
+    this.avaliacao = 0;
+    this.feedback = '';
+
+    // Exibir mensagem de sucesso como notificação (Toast)
+    const toast = await this.toastController.create({
+      message: 'Formulário enviado com sucesso!',
+      duration: 3000, // 3000 milissegundos = 3 segundos
+      position: 'top',
+      color: 'success', // Cor da notificação
+    });
+
+    toast.present();
+
+    // Atualizar a propriedade para mostrar a mensagem no template
+    this.formularioEnviado = true;
+
+    // Aguarde alguns segundos e, em seguida, limpe a mensagem
+    setTimeout(() => {
+      this.formularioEnviado = false;
+    }, 3000);
   }
 }
