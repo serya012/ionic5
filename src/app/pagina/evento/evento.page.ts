dd5fb6625b221bc2c97a1cff5f2dfafdd117e064
serya012 <ayrescarneiro01@gmail.com>
2024-01-29 19:59:03 -0300

    w

--- src/app/pagina/evento/evento.page.ts
+++ src/app/pagina/evento/evento.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
+<<<<<<< HEAD
 import { FestaService } from '../../services/festaservice/festa.service';
+=======
+import { EventoService } from '../../services/evento.service';
+>>>>>>> parent of f2de375 (fiz o crud de evento)
 
 @Component({
   selector: 'app-evento',
   templateUrl: './evento.page.html',
   styleUrls: ['./evento.page.scss'],
 })
+<<<<<<< HEAD
 export class EventPage {
   eventos: any[] = [];
   nomeEvento: string = '';
@@ -24,6 +29,14 @@ export class EventPage {
     this.novaLocalizacao = '';
     this.novoInicio = '';
     this.novoTermino = '';
+=======
+export class EventoPage {
+  nomeEvento: string = '';
+  eventos: any[] = [];
+
+  constructor(private navCtrl: NavController, private eventoService: EventoService) {
+    this.carregarEventos();
+>>>>>>> parent of f2de375 (fiz o crud de evento)
   }
 
   criarEvento() {
@@ -32,6 +45,7 @@ export class EventPage {
       return;
     }
 
+<<<<<<< HEAD
     // Altere esta linha para incluir as novas propriedades
     this.festaService.criarEvento(
       this.nomeEvento,
@@ -54,14 +68,34 @@ export class EventPage {
 
   carregarEventos() {
     this.eventos = this.festaService.obterEventos();
+=======
+    this.eventoService.criarEvento(this.nomeEvento);
+    this.carregarEventos();
+    this.nomeEvento = ''; // Limpa o campo após criar o evento
+>>>>>>> parent of f2de375 (fiz o crud de evento)
   }
 
-  editarEvento(index: number) {
-    // Lógica para edição de um evento
+  carregarEventos() {
+    this.eventos = this.eventoService.obterEventos();
   }
 
+<<<<<<< HEAD
   excluirEvento(index: number) {
     this.festaService.excluirEvento(index);
     this.carregarEventos();
   }
+=======
+  editarEvento(id: number) {
+    const novoNome = prompt('Digite o novo nome do evento:');
+    if (novoNome !== null) {
+      this.eventoService.atualizarEvento(id, novoNome);
+      this.carregarEventos();
+    }
+  }
+
+  excluirEvento(id: number) {
+    this.eventoService.excluirEvento(id);
+    this.carregarEventos();
+  }
+>>>>>>> parent of f2de375 (fiz o crud de evento)
 }
