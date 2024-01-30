9f0a29630bc4f12182a017351a39e885bf528eb5
serya012 <ayrescarneiro01@gmail.com>
2024-01-29 22:37:30 -0300

    recomeço

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BootstrapOptions } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -7,12 +8,10 @@ import { BootstrapOptions } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: 'evento', icon: 'mail' },
-    { title: 'Outbox', url: 'orcamento', icon: 'paper-plane' },
+    { title: 'Inbox', url: 'evento', icon: 'envelope' }, // Use Bootstrap icon class here
+    { title: 'Outbox', url: 'orcamento', icon: 'send' },
     { title: 'Favorites', url: 'feedback', icon: 'heart' },
     { title: 'Archived', url: 'perfil', icon: 'archive' },
-    { title: 'Trash', url: 'configuracao', icon: 'bi bi-gear' },
-    
+    { title: 'Trash', url: 'configuracao', icon: 'gear' },
   ];
- 
 }
