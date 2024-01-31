a346860b6b222400221016768324e56bfec34efd
grieblers <lucasgrieblers321@gmail.com>
2024-01-31 14:18:34 -0300

    alterações
    troquei os nomes nos app.component.ts e tbm tirei um ícone no configurações.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,10 @@ import { BootstrapOptions } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: 'evento', icon: 'envelope' }, // Use Bootstrap icon class here
-    { title: 'Outbox', url: 'orcamento', icon: 'send' },
-    { title: 'Favorites', url: 'feedback', icon: 'heart' },
+    { title: 'Eventos', url: 'evento', icon: 'envelope' }, // Use Bootstrap icon class here
+    { title: 'Orcamento', url: 'orcamento', icon: 'send' },
+    { title: 'Feedback', url: 'feedback', icon: 'heart' },
     { title: 'Archived', url: 'perfil', icon: 'archive' },
-    { title: 'Trash', url: 'configuracao', icon: 'gear' },
+    { title: 'Configuracao', url: 'configuracao', icon: 'gear' },
   ];
 }
