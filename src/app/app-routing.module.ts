4624d040544b9a4d71328785e714bbfd9291dd83
Tarcísio Ayres <ayrescarneiro01@gmail.com>
2024-01-31 15:21:11 -0300

    evento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+// /src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -7,30 +8,25 @@ const routes: Routes = [
     redirectTo: 'evento',
     pathMatch: 'full'
   },
-  
   {
     path: 'evento',
-    loadChildren: () => import('./pagina/evento/evento.module').then( m => m.EventoPageModule)
-  },
-  {
-    path: 'evento',
-    loadChildren: () => import('./pagina/evento/evento.module').then( m => m.EventoPageModule)
+    loadChildren: () => import('./pagina/evento/evento.module').then(m => m.EventoPageModule)
   },
   {
     path: 'orcamento',
-    loadChildren: () => import('./pagina/orcamento/orcamento.module').then( m => m.OrcamentoPageModule)
+    loadChildren: () => import('./pagina/orcamento/orcamento.module').then(m => m.OrcamentoPageModule)
   },
   {
     path: 'feedback',
-    loadChildren: () => import('./pagina/feedback/feedback.module').then( m => m.FeedbackPageModule)
+    loadChildren: () => import('./pagina/feedback/feedback.module').then(m => m.FeedbackPageModule)
   },
   {
     path: 'perfil',
-    loadChildren: () => import('./pagina/perfil/perfil.module').then( m => m.PerfilPageModule)
+    loadChildren: () => import('./pagina/perfil/perfil.module').then(m => m.PerfilPageModule)
   },
   {
     path: 'configuracao',
-    loadChildren: () => import('./pagina/configuracao/configuracao.module').then( m => m.ConfiguracaoPageModule)
+    loadChildren: () => import('./pagina/configuracao/configuracao.module').then(m => m.ConfiguracaoPageModule)
   }
 ];
 
--- src/app/pagina/evento/evento.module.ts
+++ src/app/pagina/evento/evento.module.ts
@@ -1,3 +1,4 @@
+// /src/app/pagina/evento/evento.module.ts
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -5,28 +6,15 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { EventoPageRoutingModule } from './evento-routing.module';
-
 import { EventoPage } from './evento.page';
-import { IonicHeaderModule } from 'ionic-header';
-
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    EventoPageRoutingModule,
-    IonicHeaderModule 
+    EventoPageRoutingModule
   ],
-  declarations: [EventoPage]
+  declarations: [EventoPage],
 })
-// /src/app/pagina/evento/evento.model.ts
-// /src/app/pagina/evento/evento.model.ts
-export interface Evento {
-  id: number;
-  nome: string;
-  data: string;
-  horaInicio: string;
-  horaTermino: string;
-  local: string;
-}
+export class EventoPageModule {}
