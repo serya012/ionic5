56d1b3c37c0a3ad8e208af1944f4a8115dc1f598
grieblers <lucasgrieblers321@gmail.com>
2024-01-31 15:03:21 -0300

    alteração
    coloquei um button de informações

--- src/app/pagina/configuracao/configuracao.page.ts
+++ src/app/pagina/configuracao/configuracao.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'; // Importe o Router se precisar redirecionar o usuário
+import { AlertController } from '@ionic/angular'; // Importe o AlertController para exibir informações
 
 @Component({
   selector: 'app-configuracao',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router'; // Importe o Router se precisar redire
 })
 export class ConfiguracaoPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private alertController: AlertController) { }
 
   ngOnInit() {
   }
@@ -23,5 +24,15 @@ export class ConfiguracaoPage implements OnInit {
     // Lógica de sair da conta
     // Por exemplo, limpar dados do usuário e redirecionar para a página inicial
     this.router.navigate(['/']); // Redirecionar para a página inicial
+  }
+
+  async informacao() {
+    const alert = await this.alertController.create({
+      header: 'Informações',
+      message: 'Aqui estão as informações que você deseja mostrar.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 }
