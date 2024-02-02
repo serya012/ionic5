43f9e4f7f2e136e4a9b4da205613d0526bc24234
gczouza <gcsouza1307@gmail.com>
2024-02-02 15:14:00 -0300

    orça

--- src/app/pagina/orcamentod/orcamentod.page.ts
+++ src/app/pagina/orcamentod/orcamentod.page.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+// orcamentod.page.ts
+
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-orcamentod',
   templateUrl: './orcamentod.page.html',
   styleUrls: ['./orcamentod.page.scss'],
 })
-export class OrcamentodPage implements OnInit {
+export class OrcamentodPage {
 
-  constructor() { }
+  categoriaSelecionada: string | null = null;
+  mostrarOpcoesMenu = false;
 
-  ngOnInit() {
+  mostrarOpcoes(categoria: string) {
+    this.mostrarOpcoesMenu = this.categoriaSelecionada !== categoria || !this.mostrarOpcoesMenu;
+    this.categoriaSelecionada = this.mostrarOpcoesMenu ? categoria : null;
   }
+  // Adicione aqui suas opções específicas de compra para cada categoria
+  // Lembre-se de ajustar conforme necessário.
+
+  opcoesBolos = [
+    { nome: 'Bolo 1', preco: 'R$20' },
+    { nome: 'Bolo 2', preco: 'R$25' },
+    // Adicione mais opções conforme necessário
+  ];
+
+  opcoesBebidas = [
+    { nome: 'Bebida 1', preco: 'R$5' },
+    { nome: 'Bebida 2', preco: 'R$8' },
+    // Adicione mais opções conforme necessário
+  ];
+
+  opcoesDecoracao = [
+    { nome: 'Enfeite 1', preco: 'R$15' },
+    { nome: 'Enfeite 2', preco: 'R$10' },
+    // Adicione mais opções conforme necessário
+  ];
+
+  opcoesComidas = [
+    { nome: 'Comida 1', preco: 'R$10' },
+    { nome: 'Comida 2', preco: 'R$12' },
+    // Adicione mais opções conforme necessário
+  ];
 
 }
