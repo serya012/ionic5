49e6850538bbb5d4d140a63ba8551f523fb96544
serya012 <ayrescarneiro01@gmail.com>
2024-02-03 15:55:59 -0300

    Create historicoeve.page.ts

---
+++ src/app/pagina/historicoeve/historicoeve.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-historicoeve',
+  templateUrl: './historicoeve.page.html',
+  styleUrls: ['./historicoeve.page.scss'],
+})
+export class HistoricoevePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
