bf648623951c45b54ca91aaea41ab0e4fd0cf2f2
gczouza <gcsouza1307@gmail.com>
2024-02-05 16:54:10 -0300

    comit

--- src/app/pagina/perfil/perfil.page.ts
+++ src/app/pagina/perfil/perfil.page.ts
@@ -1,42 +1,36 @@
 // src/app/pagina/perfil/perfil.page.ts
 
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
 
 @Component({
   selector: 'app-perfil',
   templateUrl: 'perfil.page.html',
   styleUrls: ['perfil.page.scss'],
+  providers: [Camera], // Adicione o provedor para a classe Camera aqui
 })
 export class PerfilPage {
-  fotoPerfil: string = ''; // Inicialize como uma string vazia
+  fotoPerfil: string = '';
   userEmail: string = '';
   userPassword: string = '';
 
-  constructor(private router: Router) {}
+  constructor(
+    private modalController: ModalController,
+    private camera: Camera
+  ) {}
 
-  // Função para trocar o e-mail
   changeEmail() {
     // Implemente a lógica para trocar o e-mail aqui
-    //ESTÁ SEM LÓGICA
   }
 
-  // Função para trocar a senha
   changePassword() {
     // Implemente a lógica para trocar a senha aqui
-    //ESTÁ SEM LÓGICA
-
   }
 
-  // Função para lidar com o "Esqueci a Senha"
-  async forgotPassword() {
-    // Implemente a lógica para enviar um e-mail de recuperação de senha ou outra abordagem desejada
-    // Pode incluir uma caixa de diálogo para o usuário inserir um novo password
-    // Exemplo:
-    const newPassword = prompt('Insira a nova senha');
-    if (newPassword) {
-      // Implemente a lógica para trocar a senha usando o newPassword
-      console.log('Senha alterada com sucesso:', newPassword);
-    }//NÃO RECOMENDO ESSA LÓGICA
-  }
+ 
+    
+  // Adicione a função changeProfilePicture
+ 
 }
